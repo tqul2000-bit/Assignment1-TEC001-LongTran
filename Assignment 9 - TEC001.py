4702abcace74fe5c226ad6a4ee968aa173fe59c2
tqul2000-bit <tqul2000@gmail.com>
2026-04-17 17:19:31 +0700

    Upload Assignment 10 - Task 2

--- Assignment 9 - TEC001.py
+++ Assignment 9 - TEC001.py
@@ -1,14 +1,11 @@
-def count_real_lines(filepath):
+def count_non_empty_lines(file_path):
     count = 0
     try:
-        with open(filepath, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             for line in file:
                 if line.strip():
                     count += 1
-
-        print(f"Total number of non-blank lines: {count}")
         return count
-
     except FileNotFoundError:
-        print("Error: The file does not exist.")
-count_real_lines('r')
+        return "Error: The file was not found."
+print(count_non_empty_lines('ddf.txt'))
